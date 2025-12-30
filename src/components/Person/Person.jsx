20bd9728d85ea79ef1df72cf1fa4a5ae85586dbd
Dmytro Krylov <dimononk@gmail.com>
2025-12-30 19:40:33 +0200

    fix style

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,20 @@
+/* eslint-disable prettier/prettier */
 import './Person.scss';
 
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  const partnerRole = sex === 'f' ? 'husband' : 'wife';
+  const partnerRole = sex === 'f'
+    ? 'husband'
+    : 'wife';
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
       {age && <p className="Person__age">{`I am ${age}`}</p>}
       <p className="Person__partner">
-        {isMarried ? `${partnerName} is my ${partnerRole}` : 'I am not married'}
+        {isMarried
+          ? `${partnerName} is my ${partnerRole}`
+          : 'I am not married'}
       </p>
     </section>
   );
